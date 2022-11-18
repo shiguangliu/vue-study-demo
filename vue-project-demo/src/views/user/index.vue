778023499cc3c9637d7619fa859108030cde29ad
shiguang.liu <310244110@qq.com>
2022-11-18 10:27:25 +0800

    1123213

--- vue-project-demo/src/views/user/index.vue
+++ vue-project-demo/src/views/user/index.vue
@@ -48,7 +48,7 @@
                 <el-table-column prop="updateTime" label="修改时间" width="160px" />
                 <el-table-column label="操作" width="170px">
                     <template slot-scope="scope">
-                        <el-button type="primary" size="mini" @click="handleClick('edit', scope.row)"><i
+                        <el-button type="primary" size="mini" @click="handleClick('edit', scope.row.id)"><i
                                 class="el-icon-edit">编辑</i></el-button>
                         <el-button type="danger" size="mini" @click="handleClick('updateStatus', scope.row)">
                             <i class="el-icon-setting">{{ scope.row.status == 1 ? '停用' : '启用' }}</i>
@@ -65,17 +65,16 @@
             <!-- 添加用户对话框 -->
             <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%" @close="addDialogClosed">
                 <!-- 内容主题区域 -->
-                <el-form :model="addUserInfo" :rules="addUserInfoRules" ref="addUserInfoRef" 
-                label-width="70px">
-                <el-form-item label="用户名" prop="userName">
-                    <el-input v-model="addUserInfo.userName"></el-input>
-                </el-form-item>
-                <el-form-item label="手机号码" prop="mobile">
-                    <el-input v-model="addUserInfo.mobile"></el-input>
-                </el-form-item>
-                <el-form-item label="邮箱" prop="email">
-                    <el-input v-model="addUserInfo.email"></el-input>
-                </el-form-item>
+                <el-form :model="addUserInfo" :rules="addUserInfoRules" ref="addUserInfoRef" label-width="70px">
+                    <el-form-item label="用户名" prop="userName">
+                        <el-input v-model="addUserInfo.userName"></el-input>
+                    </el-form-item>
+                    <el-form-item label="手机号码" prop="mobile">
+                        <el-input v-model="addUserInfo.mobile"></el-input>
+                    </el-form-item>
+                    <el-form-item label="邮箱" prop="email">
+                        <el-input v-model="addUserInfo.email"></el-input>
+                    </el-form-item>
                 </el-form>
                 <!-- 底部按钮区域 -->
                 <span slot="footer" class="dialog-footer">
@@ -83,12 +82,34 @@
                     <el-button type="primary" @click="handleClick('sealAddUser')">确 定</el-button>
                 </span>
             </el-dialog>
+            <!-- 修改用户对话框 -->
+            <el-dialog title="修改用户" :visible.sync="editDialogVisible" width="50%" @close="addDialogClosed">
+                <!-- 内容主题区域 -->
+                <el-form :model="editUserInfo" :rules="addUserInfoRules" ref="addUserInfoRef" label-width="70px">
+                    <el-form-item label="用户id">
+                        <el-input v-model="editUserInfo.id" disabled />
+                    </el-form-item>
+                    <el-form-item label="用户名" prop="userName">
+                        <el-input v-model="editUserInfo.userName"></el-input>
+                    </el-form-item>
+                    <el-form-item label="手机号码" prop="mobile">
+                        <el-input v-model="editUserInfo.mobile"></el-input>
+                    </el-form-item>
+                    <el-form-item label="邮箱" prop="email">
+                        <el-input v-model="editUserInfo.email"></el-input>
+                    </el-form-item>
+                </el-form>
+                <span slot="footer" class="dialog-footer">
+                    <el-button @click="editDialogVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="handleClick('updateUserInfo')">确 定</el-button>
+                </span>
+            </el-dialog>
         </el-card>
     </div>
 </template>
 
 <script>
-import { getUserListApi, updateUserStatusApi,addUserApi } from '@/api/user/index'
+import { getUserListApi, updateUserStatusApi, addUserApi, getUserInfoApi } from '@/api/user/index'
 export default {
     data() {
         var checkEmail = (rule, value, callback) => {
@@ -97,7 +118,7 @@ export default {
             }
             // 验证邮箱的正则表达式
             const regEmail = /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,5}$/
-            if(!regEmail.test(value)){
+            if (!regEmail.test(value)) {
                 callback(new Error('请输入正确的邮箱'))
             }
             callback()
@@ -108,7 +129,7 @@ export default {
             }
             // 验证手机号的正则表达式
             const regMobile = /^1[3|4|5|7|8][0-9]{9}$/
-            if(!regMobile.test(value)){
+            if (!regMobile.test(value)) {
                 callback(new Error('请输入正确的手机号'))
             }
             callback()
@@ -130,13 +151,21 @@ export default {
             ],
             // 控制添加用户对话框的显示与隐藏
             addDialogVisible: false,
+            // 控制修改用户对话框的显示与隐藏
+            editDialogVisible: false,
             addUserInfo: {
                 userName: '',
                 mobile: '',
                 email: ''
             },
+            editUserInfo: {
+                id: '',
+                userName: '',
+                mobile: '',
+                email: ''
+            },
             // 添加用户表单验证规则
-            addUserInfoRules:{
+            addUserInfoRules: {
                 userName: [
                     { required: true, message: '请输入用户名', trigger: 'blur' },
                     { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
@@ -264,7 +293,17 @@ export default {
                     this.queryInfo.status = ''
                     break
                 case 'edit':
-                    this.$router.push({ path: './edit', query: { id: data.id } })
+                    this.editDialogVisible = true
+                    // const editParams = {
+                    //     'id': data,
+                    // }
+                    // getUserInfoApi(editParams).then(res => {
+                    // if (res.data.code === 200) {
+                    //     this.editUserInfo = res.data
+                    //     this.editDialogVisible = true
+                    // } else {
+                    //     this.$message.error(res.data.msg)
+                    // }})
                     break
                 case 'updateStatus':
                     this.$message.success('修改状态成功')
@@ -281,20 +320,30 @@ export default {
                     //     }
                     // })
                     break
-                    case 'cancelAddUser':
-                        this.addDialogVisible = false
-                        break
-                    case 'sealAddUser':
-                        this.$refs.addUserInfoRef.validate((valid) => {
-                            if (valid) {
-                                this.$message.success('添加用户成功')
-                                this.addDialogVisible = false
-                                this.getUserList()
-                            } else {
-                                return false
-                            }
-                        })
-                        break
+                case 'cancelAddUser':
+                    this.addDialogVisible = false
+                    break
+                case 'sealAddUser':
+                    this.$refs.addUserInfoRef.validate((valid) => {
+                        if (valid) {
+                            this.$message.success('添加用户成功')
+                            this.addDialogVisible = false
+                            this.getUserList()
+                        } else {
+                            return false
+                        }
+                    })
+                    break
+                case 'updateUserInfo':
+                    this.$refs.addUserInfoRef.validate((valid) => {
+                        if (valid) {
+                            this.$message.success('修改用户信息成功')
+                            this.editDialogVisible = false
+                            this.getUserList()
+                        } else {
+                            return false
+                        }
+                    })
             }
         },
         addDialogClosed() {
