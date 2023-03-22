c35ae8eb6e802bf6dd7014db30f680c41fee4104
shiguang.liu <310244110@qq.com>
2023-03-22 10:44:53 +0800

    修改地址

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -126,12 +126,13 @@ export function filterDynamicRoutes(routes) {
 }
 
 export const loadView = (view) => {
-  if (process.env.NODE_ENV === 'development') {
-    return (resolve) => require([`@/views/${view}`], resolve)
-  } else {
-    // 使用 import 实现生产环境的路由懒加载
-    return () => import(`@/views/${view}`)
-  }
+  return (resolve) => require([`@/views/${view}`], resolve)
+  // if (process.env.NODE_ENV === 'development') {
+  //   return (resolve) => require([`@/views/${view}`], resolve)
+  // } else {
+  //   // 使用 import 实现生产环境的路由懒加载
+  //   return () => import(`@/views/${view}`)
+  // }
 }
 
 export default permission
